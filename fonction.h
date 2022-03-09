35d9d815cf276d8cc5faa7c9cea5c8867c78b149
AlaMdalla <alaeddine.mdalla@esprit.tn>
2022-03-09 09:33:35 +0100

    modifcation fonction .h

--- fonction.h
+++ fonction.h
@@ -1,6 +1,19 @@
-ivoid initBack(Background * b)
-void aficherBack(Background b, SDL_Surface * screen)
-void animerBackground( Background * e)
-int collisionPP( Personne p, SDL_Surface * Masque)
- void scrolling (background * b, int direction, int pasAvancement); soit void scrolling (SDL_Rect * b, int direction ,int pasAvancement);
+#include <stdio.h>
+#include <stdlib.h> 
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_mixer.h>
+#include <SDL/SDL_ttf.h>
 
+
+typedef struct background
+{
+SDL_Surface *img_bg;
+SDL_Rect position_bg;
+SDL_Rect position_collision;
+SDL_Rect scroll;
+
+void initialiser_background(background *bg);
+
+void afficher_background(background bg, SDL_Surface * ecran);
+void scrolling_bg(int clic,background *bg);

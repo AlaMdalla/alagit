17b0282c7eff6206cbde2439cd7f744043251f14
AlaMdalla <alaeddine.mdalla@esprit.tn>
2022-03-09 09:22:38 +0100

    ajout fonction.h

---
+++ fonction.h
@@ -0,0 +1,6 @@
+ivoid initBack(Background * b)
+void aficherBack(Background b, SDL_Surface * screen)
+void animerBackground( Background * e)
+int collisionPP( Personne p, SDL_Surface * Masque)
+ void scrolling (background * b, int direction, int pasAvancement); soit void scrolling (SDL_Rect * b, int direction ,int pasAvancement);
+
